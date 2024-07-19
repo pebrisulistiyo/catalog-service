26e2494b6941ff558a66b9f2dcff2ff42262ea71
Eko Pebrisulistiyo <eko.pebrisulistiyo@gmail.com>
2024-07-19 23:49:08 +0700

    Add new controller and config for tomcat embedding server

--- src/main/java/com/pebrisulistiyo/app/service/catalog/HomeController.java
+++ src/main/java/com/pebrisulistiyo/app/service/catalog/HomeController.java
@@ -1,2 +1,12 @@
-package com.pebrisulistiyo.app.service.catalog;public class HomeController {
+package com.pebrisulistiyo.app.service.catalog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String getGreeting() {
+        return "Welcome to the book catalog!";
+    }
 }
